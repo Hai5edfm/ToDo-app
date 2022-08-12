30f2bee1fe00ff0853657a29148e5b2658824625
Hai5e@GuillermoRosales <haisedfm@hai5e.com>
2022-08-12 16:02:31 -0500

    search todo css

--- src/components/ToDoSearchInput/index.tsx
+++ src/components/ToDoSearchInput/index.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+import '../../styles/components/_ToDoSearchInput.scss';
+import { SearchIcon } from '../Icons/SearchIcon';
+
 type ToDoSearchProps = {
     searchToDo: (text: string) => void;
 }
@@ -10,16 +13,20 @@ export const ToDoSearchInput: FC<ToDoSearchProps> = (
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const searchInput = document.getElementById('search-to-do') as HTMLInputElement;
-        
+
         searchToDo(searchInput.value);
         searchInput.value = '';
     }
 
     return (
-        <form onSubmit={handleSearch}>
-            <label htmlFor='search-to-do'></label>
-            <input id="search-to-do" name="search-to-do" type="text" placeholder="Search" />
-            <button type="submit">Search</button>
+        <form className='search-to-do' onSubmit={handleSearch}>
+            <label htmlFor='search-to-do' className='search-to-do__label'>Search ToDos</label>
+            <div className='search-to-do__input'>
+                <input id="search-to-do" name="search-to-do" type="text" placeholder="Search" onChange={() => handleSearch}/>
+                <button type="submit">
+                    <SearchIcon fill="#eee"/>
+                </button>
+            </div>
         </form>
     );
 }
